cb1e0955daf8d6329b8d7d1dde35785ab1803631
Bob Klaase <>
2020-12-06 12:47:34 +0000

    day4.2

--- src/Solutions/Day4.hs
+++ src/Solutions/Day4.hs
@@ -6,20 +6,46 @@ import qualified Data.Text as T
 import Data.List
 import qualified AoC.Lib as L
 
-day4 = L.Solution 4 parse part1 part2
+day4 = L.Solution 4 parse (length . part1) (length . part2)
 
 parse :: String -> [String]
 parse = map T.unpack . T.splitOn "\n\n" . T.pack
 
-part1 :: [String] -> Int
-part1 = length . filter validId . map ((map fst) .  processId)
+part1 :: [String] -> [[(String, String)]]
+part1 = filter validId . map processId
   where reqFields = filter (/= "cid") fields
-        validId = null . (\\) reqFields
+        validId = null . (\\) reqFields . map fst
 
 
-part2 :: [String] -> Int
-part2 xs = length xs
+part2 :: [String] -> [[(String, String)]]
+part2  = filter (all validateField) . part1
 
+-- valid field names
+fields = ["byr","iyr","eyr","hgt","hcl","ecl","pid","cid"]
+
+-- field validators
+validateField :: (String,String) -> Bool
+
+validateField ("byr",v) = readBetween v 1920 2002
+validateField ("iyr",v) = readBetween v 2010 2020
+validateField ("eyr",v) = readBetween v 2020 2030
+validateField ("hgt",v) = let (l,u) = span (<= '9') v in
+                            case u of "cm" -> readBetween l 150 193
+                                      "in" -> readBetween l 59 76
+                                      _ -> False
+validateField ("hcl",v) = let (pound,color) = span (== '#') v;
+                              hex = concat[['a'..'f'],['0'..'9']] in
+                            pound == "#" && length color == 6 && all (`elem` hex) color
+validateField ("ecl",v) = v `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+validateField ("pid",v) = length v == 9 && all (`elem` ['0'..'9']) v
+validateField ("cid",_) = True -- always checks out
+validateField  (_,_) = False
+
+readBetween :: String -> Int -> Int -> Bool
+readBetween s lb ub = lb <= val && val <= ub
+  where val = read s :: Int
+
+-- run parser over single id string
 processId :: String -> [(String,String)]
 processId = proc []
   where proc rs "" = rs
@@ -28,10 +54,7 @@ processId = proc []
                 nr = (fst . head) step
                 ninp = (snd . head) step
 
-
-fields = ["byr","iyr","eyr","hgt","hcl","ecl","pid","cid"]
-
--- parser
+-- parser for id to fields
 fieldP :: ReadP (String,String)
 fieldP = do
  field <- choice $ map string fields
